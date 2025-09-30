87b9c1f83fcc8990066309b236b937d17eec8b10
Jhon Jimenez <u20231c873@upc.edu.pe>
2025-09-30 14:49:20 -0500

    Me nuckeo los errores que encontramos para volverlos a subir
    * Se remplazaran por nuevos
    * -
    
    Resuelve issue: #001"

--- src/main/java/com/upc/festisolutions/repository/ImagenEventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upc.festisolutions.repository;
-
-import com.upc.festisolutions.entities.Imagenevento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ImagenEventoRepository extends JpaRepository<Imagenevento, Integer> {
-}
